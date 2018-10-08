942a2ab868cce3240389c5ef8e0165df67a22c0a
Anton Samoilov <absamoilov88@gmail.com>
2018-10-08 23:05:40 +0300

    Show active deck on main screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { compose, graphql } from 'react-apollo';
 import './App.css';
-import ArchetypesList from './components/ArchetypesList';
+import Decks from './components/Decks';
+import getActiveDeck from './graphql/getActiveDeck';
 
 class App extends Component {
+  showActiveDeck(deck) {
+    if (!deck.name) return <Decks />
+
+    return (
+      <div>
+        {deck.name}
+      </div>
+    )
+  }
+
   render() {
+    const { activeDeck } = this.props;
+
     return (
       <div className="App">
-        <ArchetypesList />
+        {this.showActiveDeck(activeDeck)}
       </div>
     );
   }
 }
 
-export default App;
+export default compose(
+  graphql(getActiveDeck, {
+    props: ({ data: { activeDeck } }) => ({
+        activeDeck
+    })
+  })
+)(App);
