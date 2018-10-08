36550b5e2863f86b60e9586cdce9d337ed897dae
Anton Samoilov <absamoilov88@gmail.com>
2018-10-08 14:26:07 +0300

    Connect apolloclient and set up hmr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:3333/graphql"
+});
+
+const render = Component => {
+  return ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    render(NextApp);
+  })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
