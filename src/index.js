d0eede07889b893dc0523ca891b50eb4d030505e
Anton Samoilov <absamoilov88@gmail.com>
2018-10-09 23:36:26 +0300

    Add new game defaults, resolvers and typedefs, along with routing

--- src/index.js
+++ src/index.js
@@ -3,29 +3,63 @@ import { ApolloProvider } from "react-apollo";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NewGame from './components/NewGame.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const defaults = {
   activeDeck: {
     __typename: 'ActiveDeck',
-    name: null
+    name: null,
+    heroImage: null
+  },
+  currentGame: {
+    __typename: 'CurrentGame',
+    opponentClass: null,
+    opponentArchetype: null,
+    mulligan: [],
+    outcome: null
   }
 };
 
 const resolvers = {
   Mutation: {
-    updateActiveDeck: (_, { id, name }, { cache }) => {
-      cache.writeData({ data: { activeDeck: { id, name, __typename: 'ActiveDeck' } } });
+    updateActiveDeck: (_, { id, name, heroImage }, { cache }) => {
+      cache.writeData({ data: { activeDeck: { id, name, heroImage, __typename: 'ActiveDeck' } } });
       return null;
     },
+    updateGameOpponentClass: (_, { opponentClass }, { cache }) => {
+      cache.writeData({ data: { currentGame: { opponentClass, __typename: 'CurrentGame' } } });
+      return null;
+    },
+    updateGameMulligan: (_, { mulligan }, { cache }) => {
+      cache.writeData({ data: { currentGame: { mulligan, __typename: 'CurrentGame' } } });
+      return null;
+    },
+    updateGameOpponentArchetype: (_, { opponentArchetype }, { cache }) => {
+      cache.writeData({ data: { currentGame: { opponentArchetype, __typename: 'CurrentGame' } } });
+      return null;
+    },
+    updateGameOutcome: (_, { outcome }, { cache }) => {
+      cache.writeData({ data: { currentGame: { outcome, __typename: 'CurrentGame' } } })
+      return null;
+    }
   }
 }
+
+const typeDefs = `
+  type Card {
+    cardId: String!,
+    cardName: String!
+  }
+`;
 
 const client = new ApolloClient({
   clientState: {
     defaults,
-    resolvers
+    resolvers,
+    typeDefs
   },
   uri: "http://localhost:3333/graphql"
 });
@@ -33,7 +67,12 @@ const client = new ApolloClient({
 const render = Component => {
   return ReactDOM.render(
     <ApolloProvider client={client}>
-      <App />
+      <Router>
+        <div>
+          <Route exact path="/" component={App} />
+          <Route path="/new-game" component={NewGame} />
+        </div>
+      </Router>
     </ApolloProvider>, 
     document.getElementById('root')
   );
