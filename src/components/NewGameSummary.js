8c60f8a86b113bc99f357328f11d8b3d1d545ab3
Anton Samoilov <absamoilov88@gmail.com>
2018-10-11 14:58:10 +0300

    Add reset game resolver

--- src/components/NewGameSummary.js
+++ src/components/NewGameSummary.js
@@ -8,8 +8,27 @@ import getArchetype from '../graphql/getArchetype';
 import createGameAndUpdateWinrate from '../graphql/createGameAndUpdateWinrate';
 import { getData } from '../helpers/storage_utils';
 import resetGame from '../graphql/resetGame';
+import getWinratesByClass from '../graphql/getWinratesByClass';
 
 class NewGameSummary extends React.Component {
+  outputWinrate(winrates) {
+    let games = 0;
+    let wins = 0;
+    let losses = 0;
+
+    winrates.forEach(wr => {
+      games += wr.games;
+      wins += wr.wins;
+      losses += wr.losses;
+    });
+
+    const winrate = `${((wins/games) * 100).toFixed(2)}%`;
+
+    return (
+      <span>{winrate} (W: {wins} / L: {losses} / G: {games})</span>
+    )
+  }
+
   outputArchetype(archetype) {
     if ( !archetype ) {
       return <span>Empty</span>;
@@ -42,7 +61,10 @@ class NewGameSummary extends React.Component {
     const deckId = getData('deck').id;
 
     return (
-      <Mutation mutation={createGameAndUpdateWinrate}>
+      <Mutation 
+        mutation={createGameAndUpdateWinrate}
+        refetchQueries={['getWinratesByClass']}
+      >
         {(createGameAndUpdateWinrate, { loading, error, data, client }) => {
           if (data && data.createGame._id) {
             this.props.resetGame()
@@ -79,11 +101,30 @@ class NewGameSummary extends React.Component {
     )
   }
 
+  outputClassWinrate(opponentClass) {
+    return <Query 
+      query={getWinratesByClass} 
+      variables={{ deckId: getData('deck').id, opponentClass }}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <span>Loading...</span>;
+        if (error) return <span>Error: {error}</span>;
+
+        return this.outputWinrate(data.getWinratesByClass);
+      }}
+    </Query>
+  }
+
   render() {
     const { currentGame } = this.props;
+    let ClassWinrate = '';
     const mulligan = currentGame.mulligan.map((card, i) => (
       <span key={`${card.cardId}_${i}`}>{card.cardName} / </span>
     ));
+
+    if (currentGame.opponentClass) {
+      ClassWinrate = this.outputClassWinrate(currentGame.opponentClass);
+    }
 
     const Archetype = this.outputArchetype(currentGame.opponentArchetype);
 
@@ -91,7 +132,8 @@ class NewGameSummary extends React.Component {
 
     return (
       <div>
-        <p><strong>Opponent class:</strong> {currentGame.opponentClass}</p>
+        <p><strong>Opponent class:</strong> {currentGame.opponentClass} {ClassWinrate}</p>
+
         <p><strong>Mulligan:</strong> {mulligan}</p>
         <p><strong>Opponent archetype:</strong> {Archetype}</p>
         <p><strong>Outcome:</strong> {currentGame.outcome}</p>
