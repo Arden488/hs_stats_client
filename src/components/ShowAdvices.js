f201c55bb6550643dfe31b67411795b7cd9089c9
Anton Samoilov <absamoilov88@gmail.com>
2019-02-20 15:54:29 +0200

    Refactor advices page

--- src/components/ShowAdvices.js
+++ src/components/ShowAdvices.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Query, compose, graphql } from 'react-apollo';
+import { compose, graphql } from 'react-apollo';
 import getCurrentGame from '../graphql/getCurrentGame';
 
 
@@ -13,15 +13,11 @@ import {
   findIndex as _findIndex,
   find as _find,
   sortBy as _sortBy,
-  uniqBy as _uniqBy
 } from 'lodash';
 
 import styled from 'styled-components'
 import updateGameOpponentDeck from '../graphql/updateGameOpponentDeck';
 import { colors, spacers, borders, fonts } from '../styles/vars';
-import { SmallButton } from '../styles/buttons';
-
-const CardList = styled.div``;
 
 const PlayedCard = styled.img`
   height: 80px;
@@ -69,7 +65,19 @@ const DeckVariant = styled.div`
   }
 `;
 
-const CardChoice = styled.button`
+const PreviewButton = styled.button`
+  button {
+    display: inline-block;
+    border: 0;
+    background: none;
+    background-color: ${colors.blocksBg};
+    width: 15px;
+    height: 15px;
+    border-radius: ${borders.borderRadius};
+  }
+`;
+
+const CardChoice = styled.div`
   position: relative;
   display: inline-block;
   background: none;
@@ -87,19 +95,6 @@ const CardChoice = styled.button`
   :focus {
     outline: 0; 
   }
-`;
-
-const CardChoiceCost = styled.span`
-  color: #ffffff;
-  display: block;
-  background: ${colors.third};
-  width: 23px;
-  line-height: 23px;
-  height: 23px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  text-align: center;
 `;
 
 const CardChoiceCount = styled.span`
@@ -138,11 +133,20 @@ const CardChoiceMechanicsRefItem = styled(CardChoiceMechanicsItem)`
   color: ${colors.third};
 `;
 
-const CardChoiceRace = styled.span`
-  font-size: 6px;
+const CardRemove = styled.button`
+  background: red;
+  border-radius: 50%;
   position: absolute;
-  left: 60%;
-  top: 13px;
+  bottom: 10px;
+  right: 0;
+  width: 15px;
+  height: 15px;
+  line-height: 15px;
+  font-size: 7px;
+  border: 0;
+  padding: 0;
+  text-align: center;
+  color: ${colors.text};
 `;
 
 const CardChoiceName = styled.span`
@@ -160,7 +164,7 @@ const CardChoiceName = styled.span`
 const CardGroup = styled.section`
   article {
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(10, 1fr);
   }
 `;
 
@@ -170,22 +174,65 @@ class ShowAdvices extends React.Component {
 
     this.state = {
       decks: [],
+      deckGroups: [],
       playedCards: [],
       opponentMana: 0,
-      activeCards: []
+      activeCards: [],
+      openedDeckGroups: [],
+      filteredCards: [],
+      currFilter: o => {
+        return o.cost <= 1
+      }
     }
 
-    this.increaseMana = this.increaseMana.bind(this);
+    // this.increaseMana = this.increaseMana.bind(this);
   }
 
-  outputAllCardsByDeck(cards, deckGroups) {
+  filterCards(filter) {
+    if (filter) {
+      this.setState({
+        filteredCards: _filter(this.state.activeCards, filter),
+        currFilter: filter
+      })
+    } else {
+      this.setState({
+        filteredCards: this.state.activeCards,
+        currFilter: {}
+      })
+    }
+  }
+
+  outputCardsFilter() {
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.filterCards({ cost: 1 })}>1</button>
+          <button onClick={() => this.filterCards({ cost: 2 })}>2</button>
+          <button onClick={() => this.filterCards({ cost: 3 })}>3</button>
+          <button onClick={() => this.filterCards({ cost: 4 })}>4</button>
+          <button onClick={() => this.filterCards({ cost: 5 })}>5</button>
+          <button onClick={() => this.filterCards({ cost: 6 })}>6</button>
+          <button onClick={() => this.filterCards({ cost: 7 })}>7</button>
+          <button onClick={() => this.filterCards({ cost: 8 })}>8</button>
+          <button onClick={() => this.filterCards({ cost: 9 })}>9</button>
+          <button onClick={() => this.filterCards(o => { return o.cost >= 10 })}>10+</button>
+          <button onClick={() => this.filterCards({ type: 'MINION' })}>Minion</button>
+          <button onClick={() => this.filterCards({ type: 'SPELL' })}>Spell</button>
+          <button onClick={() => this.filterCards({ type: 'WEAPON' })}>Weapon</button>
+          <button onClick={() => this.filterCards(false)}>All</button>
+        </div>
+      </div>
+    )
+  }
+
+  outputAllCardsByDeck(cards) {
     cards = _sortBy(cards, ['cost']);
     const cardsByGroup = _groupBy(cards, 'type');
 
     return (
-      <CardList>
-        {this.outputCardGroupsList(cardsByGroup, deckGroups)}
-      </CardList>
+      <div>
+        {this.outputCardGroupsList(cardsByGroup)}
+      </div>
     )
   }
 
@@ -194,13 +241,44 @@ class ShowAdvices extends React.Component {
     let cards = this.state.activeCards;
     newArr.push(card);
 
-    const decks = this.updateActualDecks(newArr)
-    cards = this.updateActiveCards(decks, newArr)
+    const newDecks = this.updateActualDecks(newArr)
+    cards = this.updateActiveCards(newDecks, newArr)
+    const filteredCards = _filter(cards, this.state.currFilter);
+
+    const groups = _groupBy(newDecks, (v) => {
+      return v.archetypeId.name
+    });
 
     this.setState({
       playedCards: newArr,
       activeCards: cards,
-      decks
+      decks: newDecks,
+      deckGroups: groups,
+      filteredCards
+    });
+  }
+
+  handleRemoveCard(card) {
+    let cards = this.state.activeCards;
+    
+    const newDecks = this.updateActualDecks(this.state.playedCards)
+    cards = this.updateActiveCards(newDecks, this.state.playedCards)
+
+    // const removeIndex = _findIndex(cards, { 'id': card.id });
+    // cards.splice(removeIndex, 1);
+
+
+    const filteredCards = _filter(cards, this.state.currFilter);
+
+    const groups = _groupBy(newDecks, (v) => {
+      return v.archetypeId.name
+    });
+
+    this.setState({
+      activeCards: cards,
+      decks: newDecks,
+      deckGroups: groups,
+      filteredCards
     });
   }
 
@@ -245,38 +323,38 @@ class ShowAdvices extends React.Component {
     )
   }
 
-  outputCardList(type, cards, deckGroups) {
+  outputCardList(type, cards) {
     return cards.map(card => {
       const image = getCardImageById(card.id);
       let deckSuggestion = '';
 
       // if ( (card.decks.length/this.state.decks.length)*100 <= 20 && this.state.decks.length > 1 ) {
       // if ( card.decks.length < deckGroups.length )
-      if ( Object.keys(deckGroups).length > 1 ) {
+      if ( Object.keys(this.state.deckGroups).length > 1 || card.decks.length > 1 ) {
         const displaySuggestions = _groupBy(card.decks, (v) => {
           return v.archetype
         });
-        deckSuggestion = Object.keys(displaySuggestions).map(sugg => <DeckSuggestion>{sugg}</DeckSuggestion>)
-      } else if (card.decks.length/deckGroups.length*100 <= 50) {
-        deckSuggestion = card.decks.map(deck => <DeckSuggestion>{deck.name}</DeckSuggestion>)
+        deckSuggestion = Object.keys(displaySuggestions).map(sugg => <DeckSuggestion key={sugg}>{sugg}</DeckSuggestion>)
+      } else if (card.decks.length/this.state.deckGroups.length*100 <= 50) {
+        deckSuggestion = card.decks.map(deck => <DeckSuggestion key={deck.name}>{deck.name}</DeckSuggestion>)
       }
       // }
 
       return <CardChoice 
           key={card.id} 
-          onClick={() => this.handleChooseCard(card)}
         >
           <CardChoiceName>{card.name}</CardChoiceName>
+          <CardRemove onClick={() => this.handleRemoveCard(card)}>X</CardRemove>
           {card.count > 1 ? <CardChoiceCount>{card.count}</CardChoiceCount> : ''}
-          <img src={image} alt={card.name} />
+          <img onClick={() => this.handleChooseCard(card)} src={image} alt={card.name} />
           {deckSuggestion}
         </CardChoice>
     })
   }
 
-  outputCardGroupsList(cardsByGroup, deckGroups) {
+  outputCardGroupsList(cardsByGroup) {
     return _map(cardsByGroup, (cards, type) => {
-      const cardsOutput = this.outputCardList(type, cards, deckGroups);
+      const cardsOutput = this.outputCardList(type, cards);
 
       return (
         <CardGroup key={type}>
@@ -314,10 +392,6 @@ class ShowAdvices extends React.Component {
     
     return (
       <div>
-        {/* <p>
-          Mana: {this.state.opponentMana}
-          <SmallButton onClick={this.increaseMana}>+</SmallButton>
-        </p> */}
         <p>Cards played:<br />{playedCards}</p>
       </div>
     )
@@ -327,18 +401,15 @@ class ShowAdvices extends React.Component {
     let cards = [];
 
     decks.forEach(deck => {
-      // console.log(deck)
       deck.cards.forEach(card => {
         // if ( !card.decks ) { card.decks = []; }
         const deckData = { name: deck.name, archetype: deck.archetypeId.name };
-
-        // console.log(deckData)
         
         const index = _findIndex(cards, { 'id': card.id });
         if ( index === -1 ) {
           card.decks = [deckData];
           cards.push(card)
-        } else if ( cards[index].count < 2 ) {
+        } else if ( cards[index].count < 2 && cards[index].rarity !== 'LEGENDARY' ) {
           cards[index].decks.push(deckData)
           cards[index].count++;
         } else {
@@ -365,19 +436,82 @@ class ShowAdvices extends React.Component {
     return cards;
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if(this.props.decks === prevProps.decks && this.state.decks.length === 0) {
-      const newDecks = _filter(this.props.decks, { charClass: this.props.currentGame.opponentClass });
-      const newCards = this.updateActiveCards(newDecks, this.state.playedCards);
+  componentDidMount() {
+    const oppClass = this.props.currentGame.opponentClass;
+    const newDecks = _filter(this.props.decks, { charClass: oppClass[0].toUpperCase() + oppClass.substring(1) });
+    const newCards = this.updateActiveCards(newDecks, this.state.playedCards);
+    const filteredCards = _filter(newCards, this.state.currFilter);
 
-      this.setState({
-        decks: newDecks,
-        activeCards: newCards
-      })
-    }
+    const groups = _groupBy(newDecks, (v) => {
+      return v.archetypeId.name
+    });
+
+    this.setState({
+      decks: newDecks,
+      activeCards: newCards,
+      deckGroups: groups,
+      filteredCards
+    })
   }
 
-  outputExpectedDecks(groups) {
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   if(this.props.currentGame.opponentClass && 
+  //     this.props.currentGame.opponentClass !== prevProps.currentGame.opponentClass) {
+  //     const oppClass = this.props.currentGame.opponentClass;
+  //     const newDecks = _filter(this.props.decks, { charClass: oppClass[0].toUpperCase() + oppClass.substring(1) });
+  //     const newCards = this.updateActiveCards(newDecks, this.state.playedCards);
+  //     const filteredCards = _filter(newCards, this.state.currFilter);
+
+  //     const groups = _groupBy(newDecks, (v) => {
+  //       return v.archetypeId.name
+  //     });
+
+  //     this.setState({
+  //       decks: newDecks,
+  //       activeCards: newCards,
+  //       deckGroups: groups,
+  //       filteredCards
+  //     })
+  //   }
+
+    // if(this.props.decks === prevProps.decks && this.state.decks.length === 0) {
+    //   const newDecks = _filter(this.props.decks, { charClass: this.props.currentGame.opponentClass });
+    //   const newCards = this.updateActiveCards(newDecks, this.state.playedCards);
+
+    //   this.setState({
+    //     decks: newDecks,
+    //     activeCards: newCards
+    //   })
+    // }
+  // }
+
+  expandDeckGroup(key) {
+    const openedDeckGroups = this.state.openedDeckGroups;
+    openedDeckGroups.push(key);
+
+    this.setState({
+      openedDeckGroups
+    })
+  }
+
+  outputDeck(deck) {
+    let additional = '';
+    
+    if (deck.key_features) {
+      const featArr = deck.key_features.split(/\n/)
+      const games = featArr[0].split('Total games: ')[1].slice(0,-1);;
+      additional = ( <p>{games}</p> )
+    }
+    
+    return <DeckVariant 
+      onClick={() => this.handleChooseOppDeck(deck._id)}
+      key={deck._id}>
+      {deck.name}
+      {additional}
+    </DeckVariant>
+  }
+
+  outputExpectedDecks() {
     if (this.props.currentGame.opponentDeck) {
       return (
         <div>
@@ -391,26 +525,27 @@ class ShowAdvices extends React.Component {
     return (
       <div>
         <h4>Expected decks:</h4>
-        {Object.keys(groups).map((key) => {
-          const group = groups[key];
-          let output = (
-            <DeckVariant key={key}>
+        {Object.keys(this.state.deckGroups).map((key) => {
+          const group = _sortBy(this.state.deckGroups[key], o => {
+            const featArr = o.key_features.split(/\n/)
+            const games = featArr[0].split('Total games: ')[1].slice(0,-1);
+            return parseInt(games);
+          }).reverse();
+          let output = this.state.openedDeckGroups.indexOf(key) === -1 ? (
+            <DeckVariant
+              onClick={() => this.expandDeckGroup(key)}
+              key={key}>
               {key}
               <span>{group.length}</span>
-              {group[0].archetypeId.key_features && <p>{group[0].archetypeId.key_features}</p>}
+              <PreviewButton></PreviewButton>}
             </DeckVariant>
-          );
+          ) : group.map(deck => {
+            return this.outputDeck(deck)
+          });
 
           if (group.length <= 3) {
             output = group.map(deck => {
-              return (
-                <DeckVariant 
-                  onClick={() => this.handleChooseOppDeck(deck._id)}
-                  key={deck._id}>
-                  {deck.name}
-                  {deck.key_features && <p>{deck.key_features}</p>}
-                </DeckVariant>
-              )
+              return this.outputDeck(deck)
             })
           }
 
@@ -434,22 +569,27 @@ class ShowAdvices extends React.Component {
     })
   }
 
-  updateActualDecks(cards) {
+  updateActualDecks(playedCards/*, removedCards*/) {
     const decks = this.state.decks;
     const leftDecks = [];
 
     decks.forEach((dck) => {
-      if (cards.length > 0) {
-        let av = true;
-        cards.forEach(c => {
-          const i = _findIndex(dck.cards, { 'id': c.id })
-          if (i === -1) {
-            av = false
-          }
-        })
-        if (av) {
-          leftDecks.push(dck);
+      let av = true;
+      playedCards.forEach(c => {
+        const i = _findIndex(dck.cards, { 'id': c.id })
+        if (i === -1) {
+          av = false
         }
+      })
+      // removedCards.forEach(c => {
+      //   const i = _findIndex(dck.cards, { 'id': c.id })
+      //   if (i === -1) {
+      //     av = false
+      //   }
+      // })
+
+      if (av) {
+        leftDecks.push(dck);
       }
     })
 
@@ -465,18 +605,16 @@ class ShowAdvices extends React.Component {
 
     if ( !currentGame.opponentClass || currentGame.mulligan.length < 3 || currentGame.outcome ) return false;
 
-    const groups = _groupBy(this.state.decks, (v) => {
-      return v.archetypeId.name
-    });
-
-    const cardsList = this.outputAllCardsByDeck(this.state.activeCards, groups);
+    const cardsFilter = this.outputCardsFilter();
+    const cardsList = this.outputAllCardsByDeck(this.state.filteredCards);
     const opponentInfo = this.outputOpponentGameInfo();
-    const expectedDecks = this.outputExpectedDecks(groups);
+    const expectedDecks = this.outputExpectedDecks();
 
     return (
       <div>
         {expectedDecks}
         {opponentInfo}
+        {cardsFilter}
         {cardsList}
       </div>
     )
