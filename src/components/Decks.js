4ecd48a0d27aeccb26de921723b5ea7b19acfc23
Anton Samoilov <absamoilov88@gmail.com>
2019-02-20 15:53:15 +0200

    Refactor decks selector

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Query, graphql, compose } from 'react-apollo';
 import { decodeDeck } from '../helpers/deck_codes_utils';
+import HearthstoneJSON from "hearthstonejson-client";
 import { getCardById } from '../helpers/cards_api';
 import { setData } from '../helpers/storage_utils';
+
 import updateActiveDeck from '../graphql/updateActiveDeck';
 import getDecks from '../graphql/getDecks';
+import getAllWinrates from '../graphql/getAllWinrates';
+import allOppDecks from '../graphql/allOppDecks';
+
+import { colors } from '../styles/vars';
+import { Button } from '../styles/buttons';
 
 import styled from 'styled-components'
-import { LargeButton } from '../styles/buttons';
 
 const DeckList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+`;
+
+const Deck = styled.button`
+  border: 0;
+  background: none;
+  cursor: pointer;
   text-align: center;
+  color: ${colors.text}
+
+  p {
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 class Decks extends React.Component {
@@ -39,7 +59,6 @@ class Decks extends React.Component {
     const deckData = decodeDeck(deck.code);
     deck.cards = this.fetchDeckCards(deckData.cards);
     deck.heroImage = this.fetchDeckHeroImage(deckData.heroes);
-    
     this.setActiveDeck(deck);
 
     this.props.updateActiveDeck({
@@ -47,30 +66,90 @@ class Decks extends React.Component {
     })
   }
 
+  outputDeck(deck, winrates) {
+    const deckData = decodeDeck(deck.code);
+    deck.heroImage = this.fetchDeckHeroImage(deckData.heroes);
+
+    let games = 0;
+    let wins = 0;
+
+    winrates.forEach(wr => {
+      games += wr.games;
+      wins += wr.wins;
+    });
+
+    const winrate = `${(((wins)/(games)) * 100).toFixed(2)}%`;
+
+    return <Deck
+      key={deck._id}
+      onClick={() => this.handleDeckClick({ id: deck._id, name: deck.name, code: deck.code })}>
+      <img width='150' src={deck.heroImage} alt={deck.name} />
+      <h2>{`${deck.name}`}</h2>
+      <h3>{winrate} in {games} games</h3>
+    </Deck>
+  }
+
   outputDecksList(decks) {
-    return decks.map(({ _id, name, code }) => (
-      <div key={_id}>
-        <LargeButton onClick={() => this.handleDeckClick({ id: _id, name, code })}>
-          {`${name}`}
-        </LargeButton>
-      </div>
+    return decks.map(deck => (
+      <Query key={deck._id} query={getAllWinrates} variables={{ deckId: deck._id }}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error: {error}</p>;
+
+          return this.outputDeck(deck, data.allWinrates);
+        }}
+      </Query>
     ));
   }
 
-  render() {
-    return (
-      <DeckList>
-        <h2>Choose your deck:</h2>
-        <Query query={getDecks}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading list of decks...</p>;
-            if (error) return <p>Error: {error}</p>;
+  fetchNewCardsData() {
+    const hsjson = new HearthstoneJSON();  
+    hsjson.get('latest');
+  }
 
-              return this.outputDecksList(data.allDecks);
-          }}
-        </Query>
-      </DeckList>
-    );
+  updateLocalStorage() {
+    localStorage.clear();
+
+    if (localStorage.key(0) === null) {
+      alert('Cache cleared. Fetching new data...');
+
+      this.fetchNewCardsData();
+    }
+  }
+
+  checkForLocalStorage(targetName) {
+    for (var i = 0; i < localStorage.length; i++) {
+      const name = localStorage.key(i);
+      if (name.indexOf(targetName) !== -1) return localStorage.getItem(name)
+    }
+
+    return false;
+  }
+
+  renderDecksList() {
+    return <div>
+      <h2>Choose your deck:</h2>
+      <Query query={getDecks}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading list of decks...</p>;
+          if (error) return <p>Error: {error}</p>;
+
+            return (
+              <DeckList>{this.outputDecksList(data.allDecks)}</DeckList>
+            )
+        }}
+      </Query>
+
+      <br /><br />
+
+      <Button onClick={() => this.updateLocalStorage()}>Update cards cache</Button>
+    </div>
+  }
+
+  render() {
+    if (!this.checkForLocalStorage('hsjson-')) this.fetchNewCardsData();
+
+    return this.renderDecksList();
   }
 }
 
